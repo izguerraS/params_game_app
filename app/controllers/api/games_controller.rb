05cae8e06881a28ad575bf4d27e67cf13477a020
Sam <Izguerra.sam@gmail.com>
2020-07-01 20:31:56 -0500

    URL_segment_param work

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,2 +1,34 @@
 class Api::GamesController < ApplicationController
+
+
+   
+
+
+
+
+
+
+
+
+def say_name
+    correct_number = 32
+        p "params"
+        @correct_number = params['correct_number'].to_i
+
+if @test > correct_number
+    puts "Guess again... Too high"
+  elsif @test < correct_number
+    puts "Guess again...Too low"
+  else
+    puts "Right on the money!"
+    render 'params.json.jb'
+   end
+ end
 end
+#have user guess number
+#read number
+#inform if too high, too low or just right (bernstein bears?)
+
+    
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  #EXAMPLE JSON ROUTE WITH API NAMESPACE
+  
+  namespace :api do
+    get "/params_game/:correct_number" => "games#say_name"
+  end
+
 end
