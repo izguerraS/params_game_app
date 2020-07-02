d732aa61e6e0ee3f2c9eb3d6b4d1b3626a95e7cb
Sam <Izguerra.sam@gmail.com>
2020-07-02 18:58:46 -0500

    attempt at params using post

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -3,28 +3,41 @@ class Api::GamesController < ApplicationController
 
    
 
+def body_params
+  params['number']
+correct_number = 32
+@number_guess = correct_number
 
-
-
-
-
-
-
-def say_name
-    correct_number = 32
-        p "params"
-        @correct_number = params['correct_number'].to_i
-
-if @test > correct_number
-    puts "Guess again... Too high"
-  elsif @test < correct_number
-    puts "Guess again...Too low"
+  if @number_guess > correct_number
+    p "too high"
+  elsif @number_guess < correct_number
+    p "too low"
   else
-    puts "Right on the money!"
-    render 'params.json.jb'
-   end
+    p "Correct"
+   render 'params.json.jb'
+
  end
 end
+end
+
+
+
+
+# def say_name
+#     correct_number = 32
+#         p "params"
+#         @correct_number = params['correct_number'].to_i
+
+# if @test > correct_number
+#     puts "Guess again... Too high"
+#   elsif @test < correct_number
+#     puts "Guess again...Too low"
+#   else
+#     puts "Right on the money!"
+#     render 'params.json.jb'
+#    end
+#  end
+
 #have user guess number
 #read number
 #inform if too high, too low or just right (bernstein bears?)
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
-  #EXAMPLE JSON ROUTE WITH API NAMESPACE
+  # #EXAMPLE JSON ROUTE WITH API NAMESPACE
   
-  namespace :api do
-    get "/params_game/:correct_number" => "games#say_name"
-  end
+  # namespace :api do
+  #   get "/params_game/:correct_number" => "games#say_name"
+
+namespace :api do
+  
+post "/body_params"  => "games#body_params"
+  
+ end
 
 end
